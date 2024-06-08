65d23bf79400368d9ddac28eaa5f3f886b9bd938
Jorge Thomas <jorgethomasm@hotmail.com>
2024-06-08 17:37:26 +0200

    First commit - repo started and venvs configured

---
+++ init.R
@@ -0,0 +1,15 @@
+# ======================== Wind-Energy-Yield-Forecast =========================
+#  
+# June 2024
+# @jorgethomasm
+
+
+# ========================== Load Dependencies ==========================
+
+library(reticulate)
+library(dplyr)
+library(lubridate)
+library(ggplot2)
+library(plotly)
+library(patchwork)
+library(kableExtra)
