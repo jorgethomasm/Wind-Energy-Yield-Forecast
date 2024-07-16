107df9457ae5e3e20dfeabfdf494230e7bcc4741
Jorge Thomas <jorgethomasm@hotmail.com>
2024-07-16 16:42:19 +0200

    added shiny package to image

--- R/jthomfuncs.r
+++ R/jthomfuncs.r
@@ -1,18 +1,17 @@
 # JT's functions:
 
 jthomggtheme <- theme_minimal() +
-  theme(
-        plot.title = element_text(size = 14,
+  theme(plot.title = element_text(size = 14,
                                   hjust = 0.5),
         axis.ticks = element_line(),
         # Y-axis:
         axis.title.y = element_text(angle = 0,
-                                    size = 12,
+                                    size = 11,
                                     vjust = 0.5),
         axis.text.y  = element_text(size = 11),
         # X-axis:
         axis.title.x = element_text(angle = 0,
-                                    size = 12,
+                                    size = 11,
                                     vjust = 0.5),
         axis.text.x  = element_text(angle = 0,
                                     size = 11))
